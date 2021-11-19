de37516fc6cd2194ed8084de2533742795a63684
ChristineWasike <christinewasike22@gmail.com>
2021-11-19 09:48:14 +0300

    Finished the basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:save/screens/wrapper.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Wrapper(),
   
     );
   }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:save/screens/home/home.dart';
+import 'package:save/screens/authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Return either home or authenticate widget
+    // return either home or authenticate widget
     return Home();
   }
 }
