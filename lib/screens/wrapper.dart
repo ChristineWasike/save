8103d5446bf251929501dccad4cd06a83f8f3b3d
ChristineWasike <christinewasike22@gmail.com>
2021-11-21 12:05:36 +0300

    Paced with christine branch

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:save/screens/home/home.dart';
+// import 'package:save/screens/home/home.dart';
 import 'package:save/screens/authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either home or authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
