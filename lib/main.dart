13fc970e552b66f6e798c1390c445cb21eb716cf
lavinakathambi <lavinakathambi.m@gmail.com>
2021-11-24 16:58:06 +0200

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:save/screens/wrapper.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  List<PageViewModel> getPages() {
+    return [
+      PageViewModel(
+        image: Image.network('https://unsplash.com/photos/ACWCQs6KXcw'),
+        title: 'Dummy Text',
+        body: 'Hello',
+        footer: Text('Hello again!'),
+      ),
+      PageViewModel(
+        image: Image.network('https://unsplash.com/photos/ACWCQs6KXcw'),
+        title: 'Dummy Text',
+        body: 'Hello',
+        footer: Text('Hello again!'),
+      ),
+      PageViewModel(
+        image: Image.network('https://unsplash.com/photos/ACWCQs6KXcw'),
+        title: 'Dummy Text',
+        body: 'Hello',
+        footer: Text('Hello again!'),
+      ),
+    ];
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Wrapper(),
-  
+      home: Scaffold(
+        body: IntroductionScreen(
+          done: Text(
+            'Done',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          onDone: () {},
+          pages: getPages(),
+          globalBackgroundColor: Colors.white,
+        ),
+      ),
     );
   }
 }
-
-
-
